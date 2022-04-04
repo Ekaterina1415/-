d8c8972f85e87ead5fa091a12b9994ec511e15b1
kibertech <lukanindan@gmail.com>
2022-04-04 17:35:22 +0500

    added Hello

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+	// int x = 0;
+	// printf("Введите ваш возраст ");
+	// scanf("%d", &x);
+	// printf("Res: %d\n", x);
+	// return 0;
+	printf("Hello!");
+}
+int sum(int x int c)
+{
+	int res = c + x;
+	return 0;
