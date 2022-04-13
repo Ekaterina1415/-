90d7a1bd4967be126649a61fc771cece767d044e
Kate Vozilkina <vozilkinaka@mail.ru>
2022-04-13 18:14:39 +0500

    команда вайл

--- main.c
+++ main.c
@@ -4,15 +4,33 @@
 
 int main(int argc, char const *argv[])
 {
-	// int x = 0;
-	// printf("Введите ваш возраст ");
-	// scanf("%d", &x);
-	// printf("Res: %d\n", x);
-	// return 0;
-	printf("Hello!");
-	printf("World");
-}
-int sum(int x int c)
-{
-	int res = c + x;
+	// char a[10] = "asdf";
+	// for(int i = 0; i < 10; i++)
+	// {
+	// 	printf("%c", a[9-i]);
+
+	// }
+	// string aaa = ""
+	char a[10] = "asdf";
+	int i = 0;
+	while(i < 10)
+	{
+		printf("%c", a[i]);
+		i++;
+
+	}
+// 	// int x = 0;
+// 	// printf("Введите ваш возраст ");
+// 	// scanf("%d", &x);
+// 	// printf("Res: %d\n", x);
+// 	// return 0;
+	// printf("%s\n", a);
 	return 0;
+	// printf("Hello!");
+	// printf("World\n");
+}
+// int sum(int x,int c)
+// {
+// 	int res = c + x;
+// 	return res;
+// }
